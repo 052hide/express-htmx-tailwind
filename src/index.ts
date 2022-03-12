b8bbfc48d3e0cd206636ab3a52a5a9048fa458f7
052hide <hidetoshi1990@gmail.com>
2022-03-13 01:24:03 +0900

    add htmx

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,10 @@ const app = express()
 app.set('views', path.join(__dirname, '/views'))
 app.set('view engine', 'ejs')
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(
+  '/htmx',
+  express.static(path.join(__dirname, '../node_modules/htmx.org/dist'))
+)
 
 app.listen(3000, () => {
   console.log('Start on port 3000.')
@@ -19,4 +23,9 @@ app.get('/dashboard', (req, res) => {
   res.render('pages/dashboard', {
     username: 'aaaaa',
   })
+})
+
+app.post('/clicked', (req, res) => {
+  console.log('aaa')
+  res.render('pages/index')
 })
