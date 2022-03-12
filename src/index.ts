73f6a8beeb449616f376fe45f457a847fbfa17c5
052hide <hidetoshi1990@gmail.com>
2022-03-13 00:05:05 +0900

    return ejs

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,15 @@
 import express from 'express'
+import path from 'path'
 
 const app = express()
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.set('views', path.join(__dirname, '/views'))
+app.set('view engine', 'ejs')
 
 app.listen(3000, () => {
   console.log('Start on port 3000.')
 })
 
-app.get('/', (_req: express.Request, res: express.Response) => {
-  res.send('Hello World!')
+app.get('/', (req, res) => {
+  res.render('pages/index')
 })
