14bdeadb24f4d0021b79db6382f3204b3fae81d1
052hide <hidetoshi1990@gmail.com>
2022-03-13 00:39:52 +0900

    add static files

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ const app = express()
 
 app.set('views', path.join(__dirname, '/views'))
 app.set('view engine', 'ejs')
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.listen(3000, () => {
   console.log('Start on port 3000.')
